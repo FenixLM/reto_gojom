94a311863c958ec6fdca66b5a5652b839fa7d00a
Felix Lamadrid Morales <felix.lamadrid97@gmail.com>
2022-10-21 17:57:22 -0500

    Integracion de coma en numeros y mejora de interfaz (#11)

--- js/index.js
+++ js/index.js
@@ -1,65 +1,75 @@
 import data from './data.js'
-import  creationContentMap  from './dom.js'
+import creationContentMap from './dom.js'
 
 
 
 async function initMap() {
 
-    const collectionData = await  data('../data/items.json');
-    const positions = collectionData.properties;
-    let returnDom = {}
+  const collectionData = await data('../data/items.json');
+  const positions = collectionData.properties;
+  let returnDom = {}
 
-    const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 13,
-      center: { lat: collectionData.central_geo_point.latitude, lng: collectionData.central_geo_point.longitude },
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 13,
+    center: { lat: collectionData.central_geo_point.latitude, lng: collectionData.central_geo_point.longitude },
+  });
+
+
+  const infoWindow = new google.maps.InfoWindow({
+    content: "",
+    disableAutoPan: true,
+  });
+
+
+  const markers = positions.map((position, i) => {
+    const textLabel = `${convertiFormatoMoneda(position.usd_price)}`;
+
+    const marker = new google.maps.Marker({
+      position: { lat: position.location.latitude, lng: position.location.longitude },
+      label: {
+        text: textLabel,
+        className: 'maker-general'
+      },
+      icon: ' '
     });
 
 
-    const infoWindow = new google.maps.InfoWindow({
-      content: "",
-      disableAutoPan: true,
+    marker.addListener("click", () => {
+      infoWindow.setContent(textLabel);
+      infoWindow.open(map, marker);
+
+      returnDom = creationContentMap(position);
+
+      document.getElementById('dataPunto').style.display = 'block'
+      document.getElementById('sinDataPunto').style.display = 'none'
     });
+    return marker;
+  });
 
-    
-    const markers = positions.map((position, i) => {
-      const textLabel = `${position.usd_price}`;
+  // console.log(returnDom.arrayOtherCharacteristExtra);
 
-      const marker = new google.maps.Marker({
-        position: { lat: position.location.latitude, lng: position.location.longitude },
-        label:{
-          text: textLabel,
-          className: 'maker-general'
-        },
-        icon: ' '
-      });
-  
+  const content_otherCharacterist_btn = document.querySelector('.content_otherCharacterist_btn');
 
-      marker.addListener("click", () => {
-        infoWindow.setContent(textLabel);
-        infoWindow.open(map, marker);
-
-         returnDom = creationContentMap(position);
-      });
-      return marker;
-    });
+  content_otherCharacterist_btn.addEventListener('click', () => {
+    verMasCaracteristicas(returnDom)
+  });
 
 
-    const content_otherCharacterist_btn = document.querySelector('.content_otherCharacterist_btn');
+  new markerClusterer.MarkerClusterer({ markers, map });
 
-    content_otherCharacterist_btn.addEventListener('click', ()=>{
-      verMasCaracteristicas(returnDom)
-    });
-  
- 
+}
 
 
-    new  markerClusterer.MarkerClusterer({ markers, map });
-  }
-  
-  
-  
-  window.initMap = initMap;
 
-  function verMasCaracteristicas(characterist){
-console.log(characterist);
-  }
+window.initMap = initMap;
+
+function verMasCaracteristicas(characterist) {
+  console.log(characterist);
+
+  const sheet = document.querySelector('#sheetCharacterist');
+  sheet.classList.add('active')
+}
+
+function convertiFormatoMoneda(cantidad) {
+  return cantidad.toLocaleString('es-PE')
+}
